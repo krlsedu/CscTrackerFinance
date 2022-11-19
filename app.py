f47969be4d28b127b9c9a6135ef685d44772378c
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-11-18 22:38:13 -0300

    Load balancer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
+from service.LoadBalancerRegister import LoadBalancerRegister
 from service.TransactionHandler import TransactionHandler
 
 app = Flask(__name__)
@@ -9,6 +10,16 @@ transaction_handler = TransactionHandler()
 
 # group by endpoint rather than path
 metrics = PrometheusMetrics(app, group_by='endpoint')
+
+balancer = LoadBalancerRegister()
+
+
+def schedule_job():
+    balancer.register_service('finance')
+
+
+t1 = threading.Thread(target=schedule_job, args=())
+t1.start()
 
 
 @app.route('/transaction', methods=['POST'])
